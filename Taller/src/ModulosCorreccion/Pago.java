cc930d8acba4aaffb3c166ad1107244577ba0890
Miguel <angelmigue.cespedes@gmail.com>
2020-06-24 16:11:08 -0500

    Modificada la clase Pago para convertirla a interface

--- Taller/src/ModulosCorreccion/Pago.java
+++ Taller/src/ModulosCorreccion/Pago.java
@@ -1,15 +1,7 @@
+﻿
+package ModulosCorreccion;
 
-package Modulos;
-
-public class Pago {
-    //atributos aquí
-    public void realizarCobro(double monto){
-        //cargar el monto de compra al medio de pago
-    }
-    public void calcularImpuestosFactura(){
-        //calcula los impuestos asociados a la compra
-    }
-    public void generarFactura(){
-        //generar una nueva factura
-    }
+public interface Pago {
+    
+    public void realizarCobro(double monto);
 }

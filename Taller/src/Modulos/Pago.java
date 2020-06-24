03830dec377a27d19fc33526330f0d04b0037e24
Luis <Lurochav@espol.edu.ec>
2020-06-24 10:11:29 -0500

    Modulos a corregir

---
+++ Taller/src/Modulos/Pago.java
@@ -0,0 +1,15 @@
+
+package Modulos;
+
+public class Pago {
+    //atributos aquí
+    public void realizarCobro(double monto){
+        //cargar el monto de compra al medio de pago
+    }
+    public void calcularImpuestosFactura(){
+        //calcula los impuestos asociados a la compra
+    }
+    public void generarFactura(){
+        //generar una nueva factura
+    }
+}

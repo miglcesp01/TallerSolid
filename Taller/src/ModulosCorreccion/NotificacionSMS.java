5700f2e37fa4916017d8a3a21f6e9234f8194704
miglcesp01 <66740053+miglcesp01@users.noreply.github.com>
2020-06-25 16:27:11 -0500

    update NotificacionSMS

---
+++ Taller/src/ModulosCorreccion/NotificacionSMS.java
@@ -0,0 +1,12 @@
+package ModulosCorreccion;
+
+public class NotificacionSMS implements Notificacion {
+    private Pago pago;
+
+    @Override
+    public void notificar() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}

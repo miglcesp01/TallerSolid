ee77171561839afade58bf892dcc45bec2681dfa
miglcesp01 <66740053+miglcesp01@users.noreply.github.com>
2020-06-25 16:25:38 -0500

    Update Factura.java

---
+++ Taller/src/ModulosCorreccion/Factura.java
@@ -0,0 +1,15 @@
+package ModulosCorreccion;
+
+public class Factura {
+    public double calcularImpuestosFactura(double montoTotal){
+        return montoTotal*0.12;
+        //Calcular impuestos asociados a la compra
+    }
+    public void generarFactura(){
+        //implementar la funcion calcularImpuestosFactura
+        //Con los impuestos de la factura se generara dicha factura
+        //Generar las facturas
+        
+    }
+    
+}

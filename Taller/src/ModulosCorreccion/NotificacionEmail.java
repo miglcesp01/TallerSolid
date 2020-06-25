25cb3b2ab7aef57842101aa821cfefe5978f2448
miglcesp01 <66740053+miglcesp01@users.noreply.github.com>
2020-06-25 16:26:38 -0500

    Actualizacion Notificacion.Email

---
+++ Taller/src/ModulosCorreccion/NotificacionEmail.java
@@ -0,0 +1,12 @@
+package ModulosCorreccion;
+
+public class NotificacionEmail implements Notificacion{
+    private Pago pago;
+
+    @Override
+    public void notificar() {
+        // TODO Auto-generated method stub
+
+    }
+
+}

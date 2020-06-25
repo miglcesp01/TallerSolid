e1fbbeafe6e576e17808b645f04eaf2252aaccb0
Miguel <angelmigue.cespedes@gmail.com>
2020-06-25 16:21:40 -0500

    Modificacion de archivos articulo y PagoEfectivo

---
+++ Taller/src/ModulosCorreccion/Articulo.java
@@ -0,0 +1,13 @@
+package ModulosCorreccion;
+
+public class Articulo {
+    private String codigo;
+    private double precio;
+    private String nombre;
+
+    public Articulo(){
+        //constructor
+    }
+
+    //getters y setters
+}
---
+++ Taller/src/ModulosCorreccion/PagoEfectivo.java
@@ -0,0 +1,11 @@
+package ModulosCorreccion;
+
+public class PagoEfectivo implements Pago{
+
+    @Override
+    public void realizarCobro(double monto) {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
